c28b6f2bd85a3705e457785d89a04909eddb98ea
Alyssa DeMetro <leanndemetro@gmail.com>
2021-10-28 16:28:54 -0400

    typo change

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
   // Use the CDN in production and localhost for development.
-  assetPrefix: isProd ? 'https://cdn.statically.io/gh/leanndemetro/leanndemetro.github.io/githubactions' : '',
+  assetPrefix: isProd ? 'https://cdn.statically.io/gh/leanndemetro/leanndemetro.github.io/githubActions' : '',
 
   reactStrictMode: true,
 }
