266f77c60602485be24d3bd7d24d497525df21dd
Alyssa DeMetro <leanndemetro@gmail.com>
2021-10-28 16:18:49 -0400

    url update

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
   // Use the CDN in production and localhost for development.
-  assetPrefix: isProd ? 'https://cdn.statically.io/gh/NaveenDA/naveenda.github.io/gh-pages/' : '',
+  assetPrefix: isProd ? 'https://cdn.statically.io/gh/leanndemetro/leanndemetro.github.io/githubactions' : '',
 
   reactStrictMode: true,
 }
